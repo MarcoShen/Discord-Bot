ba3705fe4fb21764a3c35d9f0e170c952c93fea5
Marco <marco.shen107@gmail.com>
2021-08-19 22:27:11 -0400

    finished checkTopPlays

--- commands/checkTopPlays.js
+++ commands/checkTopPlays.js
@@ -128,12 +128,7 @@ function getMaxCombo(t, mapID){
 
 API_URL = 'https://osu.ppy.sh/api/v2'
 TOKEN_URL = 'https://osu.ppy.sh/oauth/token'
-TOKEN_DATA = {
-    'client_id': 7946,
-    'client_secret': 'DNyljviZudpOfRdybnVsivyqMS8wV3mmAPFqu3Oh',
-    'grant_type': 'client_credentials',
-    'scope': 'public'
-};
+
 
 function getToken(){
     const promise = axios.post(TOKEN_URL, data=TOKEN_DATA)
